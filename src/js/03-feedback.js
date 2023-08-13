e0a0d80dccc0765fe753ed3f587d9b5fb0c2f19a
alkatsalex <alkatsalex@ukr.net>
2023-08-13 23:44:00 +0300

    done with gpt

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,6 @@
 var throttle = require('lodash.throttle');
 
-const STORAGE_KAY = "feedback-form-state"
+const STORAGE_KEY = "feedback-form-state"
 
 const form = document.querySelector('.feedback-form')
 
@@ -18,21 +18,32 @@ function onSubmit(e) {
 
 e.currentTarget.reset()
 
-    console.log(JSON.parse(localStorage.getItem(STORAGE_KAY)))
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)))
 
-    localStorage.removeItem(STORAGE_KAY)
+    localStorage.removeItem(STORAGE_KEY)
 }
 
 function onInput(e) {
     formData[e.target.name] = e.target.value
-localStorage.setItem(STORAGE_KAY, JSON.stringify(formData))
+// localStorage.setItem(STORAGE_KAY, JSON.stringify(Object.assign({}, formData)))
+saveFormData()
 }
 
 function onEnter() {
-    const sevedMassege = localStorage.getItem(STORAGE_KAY)
+    const sevedMassege = localStorage.getItem(STORAGE_KEY)
     if (sevedMassege) {
       form.email.value = JSON.parse(sevedMassege).email || ''
       form.message.value = JSON.parse(sevedMassege).message || ''
     } 
     
+    }
+
+    function saveFormData() {
+        const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+        
+        // Перевіряємо, чи поля вже існують, тоді лише оновлюємо їх значення
+        savedData.email = formData.email || savedData.email || '';
+        savedData.message = formData.message || savedData.message || '';
+        
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(savedData));
     }
